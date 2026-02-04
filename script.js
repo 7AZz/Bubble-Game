b80e8bfac1f0fda54dc4d39dff25a6cf84b5cead
Tanveer Alam <ansaritaz78601@gmail.com>
2026-02-04 20:59:55 +0530

    completed the backend

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-var timer = 60;
+var timer = 30;
 var score = 0;
+var hitrn = 0;
 
-
-
-function increaseScore(){
-    score += 10;
-    document.querySelector("#scoreval").textContent = score;
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
 }
 
-function getNewHit(){
-    var rn = Math.floor((Math.random()*10));
-    document.querySelector("#hitvar").textContent = rn;
+function getNewHit() {
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitvar").textContent = hitrn;
 }
 
-function makeBubble(){
-    var clutter = "";
+function makeBubble() {
+  var clutter = "";
 
-for(var i = 1;  i< 169; i++){
-    var rn = Math.floor(Math.random()* 10)
+  for (var i = 1; i < 169; i++) {
+    var rn = Math.floor(Math.random() * 10);
     clutter += `<div class="bubble">${rn}</div>`;
+  }
+
+  document.querySelector("#pbtm").innerHTML = clutter;
 }
 
-document.querySelector("#pbtm").innerHTML = clutter;
+function runTimer() {
+  var timerint = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").innerText = timer;
+    } else {
+      clearInterval(timerint);
+      document.querySelector("#pbtm").innerHTML = `<h1>Game Over!</h1>`;
+    }
+  }, 1000);
 }
 
-function runTimer(){
-    var timerint = setInterval(function(){
-        if(timer>0){
-            timer--; 
-            document.querySelector("#timerval").innerText = timer; 
-        }
-        else{
-            clearInterval(timerint);
-        }
-    }, 1000);
-}
-
-document.querySelector("#pbtm").addEventListener("click")
+document.querySelector("#pbtm").addEventListener("click", function (details) {
+  var clickednum = Number(details.target.textContent);
+  if (clickednum === hitrn) {
+    increaseScore();
+    makeBubble();
+    getNewHit();
+  }
+});
 
 runTimer();
 makeBubble();
